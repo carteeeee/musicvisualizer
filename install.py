87aef18bbce4d96e03d183a45a3695405b60e309
Mr. Ubun2 <77160595+MrUbun2@users.noreply.github.com>
2021-05-29 13:03:42 -0500

    install ffmpeg-bar
    i forgot ffmpeg-bar on the very first commit!
    lol

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 from colorama import Fore, Back, Style
 import os
-print(Style.DIM+"`"+Style.RESET_ALL+Fore.MAGENTA+"pip3"+Style.RESET_ALL+Fore.BLUE+Style.BRIGHT+" install -r requirements.txt"+Style.RESET_ALL+Style.DIM+"`"+Style.BRIGHT)
+print(Style.DIM+"`"+Style.RESET_ALL+Fore.MAGENTA+"pip3"+Style.RESET_ALL+Fore.BLUE+Style.BRIGHT+" install -r requirements.txt"+Style.RESET_ALL+Style.DIM+"`"+Style.RESET_ALL)
 os.system("pip3 install -r requirements.txt")
+print(Style.DIM+"`"+Style.RESET_ALL+Fore.MAGENTA+"npm"+Style.RESET_ALL+Fore.BLUE+Style.BRIGHT+" install --global ffmpeg-progressbar-cli"+Style.RESET_ALL+Style.DIM+"`"+Style.RESET_ALL)
+os.system("npm install --global ffmpeg-progressbar-cli")
 print(Style.RESET_ALL+Style.BRIGHT+Fore.CYAN+"You're all good to go!"+Style.RESET_ALL)
